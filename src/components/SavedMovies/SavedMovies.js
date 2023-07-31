55a91a1a1cea6cf76a050ec3fd2ead3eaad8090c
Eugeniy Razveev <razveev.evg@gmail.com>
2023-07-31 23:23:56 +0300

    s

--- src/components/SavedMovies/SavedMovies.js
+++ src/components/SavedMovies/SavedMovies.js
@@ -13,7 +13,7 @@ function SavedMovies({ savedMoviesList, setSavedMoviesList, searchMovies, onCard
     if (JSON.parse(localStorage.getItem('saved-movies')) !== null && JSON.parse(localStorage.getItem('saved-movies')).length > 0) {
       console.log('меня изменили в saved-movies, сейчас я ', isChecked);
       console.log(JSON.parse(localStorage.getItem('saved-movies')));
-      console.log(JSON.parse(localStorage.getItem('savedMoviesSearchQuery')));
+      console.log(localStorage.getItem('savedMoviesSearchQuery'));
       setSavedMoviesList(filterMoviesByKeyword(JSON.parse(localStorage.getItem('saved-movies'))), localStorage.getItem('savedMoviesSearchQuery'), isChecked);
     }
   }, [isChecked]);
