349956ee3a4b51ec0fc55d72001f5aa96a818513
Eugeniy Razveev <razveev.evg@gmail.com>
2023-08-04 23:04:56 +0300

    Header при логине

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -12,7 +12,44 @@ function Header({ loggedIn, onHamburgerClick, isOpenHamburgerMenu }) {
   return (
     <header className={`header ${location.pathname !== '/' ? 'header_background_white' : ''}`}>
       <div className='header__content'>
-        {location.pathname !== '/'
+        {
+          location.pathname === '/'
+          ?
+          loggedIn &&
+          <>
+            <Logo />
+            {!isOpenHamburgerMenu &&
+              <>
+                <div className='header__navigation'>
+                  <Navigation />
+                  <Account />
+                </div>
+                <Account />
+              </>
+            }
+            <button
+              className='header__button-menu'
+              type='button'
+              onClick={onHamburgerClick}
+              aria-label='Меню сайта'
+            >
+            </button>
+          </>
+          :
+          <nav className='header__menu'>
+            <ul className='header__items'>
+              <li className='header__item'>
+                <Link className='header__register-link' to='/signup'>Регистрация</Link>
+              </li>
+              <li className='header__item'>
+                <Link className='header__login-link' to='/signin'>
+                  <button className='header__button' type='button'>Войти</button>
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        }
+        {/*location.pathname !== '/'
         ?
         <>
           <Logo />
@@ -50,7 +87,7 @@ function Header({ loggedIn, onHamburgerClick, isOpenHamburgerMenu }) {
             </nav>
           }
         </>
-        }
+        */}
       </div>
     </header>
   );
